f323ebd7e2474928f925bd11887e1049275e30ea
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-02-15 21:35:55 -0300

    troca para tela do quiz

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function selectQuiz(){
+    let addHidden = document.querySelector(".list-quizz-1");
+    addHidden.classList.add("hidden");
+    addHidden = document.querySelector(".list-quizz-2");
+    addHidden.classList.add("hidden");
+    addHidden = document.querySelector(".create-quizz");
+    addHidden.classList.add("hidden");
+    addHidden = document.querySelector(".pag-quizz");
+    addHidden.classList.remove("hidden");
+}
